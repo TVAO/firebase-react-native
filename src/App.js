b6d2f47e01bc23138bff84d3b48aac7bcd8fc108
TVANO <thorolesen14@gmail.com>
2018-03-22 21:47:52 +0100

    Created simple Firebase example of real-time update of data in React

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
 
+// React is used to handle state using 'setState' and 'render' 
 class App extends Component {
+
+  // Initialize component with state 
+  // Sets initial data 
+  constructor() {
+    super(); 
+    this.state = { // State of component 
+      speed: 10 // Property 
+    };
+  }
+
+  // Lifecycle method called once right after component is rendered to DOM 
+  // Great place for real-time database to asynchronously download data from database 
+  componentDidMount() {
+    const rootFirebaseRef = firebase.database().ref(); //.child('react');
+    const speedRef = rootFirebaseRef.child('speed');
+
+    // Firebase realtime listener
+    speedRef.on('value', snap => { 
+      // On method allows to synchronize real-time 
+      // Attached to reference in database 
+      // Everytime data is changed at speed location in firebase db, provide snap callback with updated/new data
+      this.setState({ // Re render app with new data 
+        speed: snap.val()
+      });
+      console.log(snap.val());
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1> {this.state.speed} </h1>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -9,18 +9,36 @@ import * as firebase from 'firebase';
 // Load environment variables 
 dotenv.config()  
 
+// Initialize firebase 
 const config = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}.firebaseapp.com`,
-    databaseURL: `https://${process.env.REACT_APP_FIREBASE_DATABASE_ID}.firebaseio.com`,
-    storageBucket: "",
-    messagingSenderId: `${process.env.REACT_APP_FIREBASE_MESSAGING_ID}`
+    databaseURL: `https://${process.env.REACT_APP_FIREBASE_PROJECT_ID}.firebaseio.com`,
+    projectId: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}`,
+    storageBucket: `${process.env.REACT_APP_FIREBASE_PROJECT_ID}.appspot.com`,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_ID
   };
-console.log(config)
 firebase.initializeApp(config);
 
 // Get a reference to the database service 
 const database = firebase.database(); 
+
+// Example
+//console.log(database.ref('speed'));
+
+
+// Firebase Example 
+//const dbRef = database.ref().child('data');
+
+// Real time database listener 
+// Everytime value updates at data location 'data' => call setState => re-render app 
+// Manages state synchronization one place in setState 
+// dbRef.on('value', snapchot => {
+//     this.setState({
+//         data: snapshot.val()
+//     });
+// });
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
